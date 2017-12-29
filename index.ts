25ab51ba75179b201472af6bf41313a32f426d29
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-12-29 23:59:03 +0200

    up

--- index.ts
+++ index.ts
@@ -1,27 +1,42 @@
+// tslint:disable-next-line:no-var-requires
+const fontJson = require("./fonts/Open_Sans_Bold.json");
 import "./index.scss";
 
-import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import * as THREE from "three";
+(window as any).THREE = THREE;
+import "./controls/OrbitControls";
 
-const scene = new Scene();
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
 
-const renderer = new WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 5);
+camera.lookAt(scene.position);
 
-camera.position.z = 5;
+// const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+// const geometry = new THREE.TextGeometry("Hello amigo", {
+//   font: new THREE.Font(fontJson),
+//   size: 80,
+//   height: 5,
+//   curveSegments: 12,
+//   bevelEnabled: true,
+//   bevelThickness: 10,
+//   bevelSize: 8,
+// });
+// const mesh = new THREE.Mesh(geometry, material);
 
-function animate() {
-  requestAnimationFrame(animate);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mesh = new THREE.Mesh(geometry, material);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+scene.add(mesh);
 
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", () => {
   renderer.render(scene, camera);
-}
-animate();
+});
+
+renderer.render(scene, camera);
